0847604ae97ee31ff2842d2556df31dfccc4d456
L0LiS <bihuy027@gmail.com>
2022-11-21 19:01:54 +0700

    add Product setting

--- js/renderSetting.js
+++ js/renderSetting.js
@@ -1,3 +1,5 @@
+import products from "./productList.js";
+
 let accounts = JSON.parse(localStorage.getItem("AccountList"));
 let htmlString = ``;
 
@@ -7,6 +9,12 @@ const page = params.get("p");
 const accountsPlace = document.querySelector(
   ".setting-section__accounts-wrapper"
 );
+const productsPlace = document.querySelector(
+  ".setting-section__products-wrapper"
+);
+
+const addProductBtn = document.querySelector(".setting-section__add-btn-wrapper");
+
 const logoutBtn = document.querySelector(".log-out-btn");
 const type = document.querySelector(".type-text");
 
@@ -38,13 +46,83 @@ function renderAccounts(accounts) {
   });
 }
 
+function renderProducts(products) {
+  htmlString = ``;
+  products.forEach(product => {
+    htmlString += `
+    <div class="setting-section__product-wrapper" id="${product.id}">
+      <div class="setting-section__info-wrapper">
+          <div class="setting-section__img-wrapper">
+              <img src="${product.img}" alt="">
+          </div>
+          <div class="setting-section__meta-wrapper">
+              <div class="setting-section__meta-text">${product.name}</div>
+              <div class="setting-section__meta-price">${product.Price}<span class="currency">đ</span></div>
+          </div>
+      </div>
+      <div class="setting-section__btn-wrapper">
+          <span class="setting-section__delete-btn" id="${product.id}">Delete</span>
+          <span class="setting-section__update-btn" id="${product.id}">Update</span>
+      </div>
+    </div>`
+  });
+  productsPlace.innerHTML = htmlString;
+
+  const addBtn = document.querySelector(".setting-section__add-btn-wrapper");
+  addBtn.addEventListener('click', () => {
+    ManageControlHidden(false);
+  })
+
+  const deleteBtns = document.querySelectorAll(
+    ".setting-section__delete-btn"
+  );
+  deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener("click", () => {
+      updateProducts(products.findIndex(x => x.id === Number(deleteBtn.id)));
+    });
+  });
+
+  const updateBtns = document.querySelectorAll(
+    ".setting-section__update-btn"
+  );
+  updateBtns.forEach((updateBtn) => {
+    updateBtn.addEventListener("click", () => {
+      ManageControlHidden(false);
+    });
+  });
+
+  const ManageCloseBtn = document.querySelector(".setting-section__manage-close");
+  ManageCloseBtn.addEventListener('click', () => {
+    ManageControlHidden(true);
+  })
+}
+
+function updateProducts(pos) {
+  products.splice(pos, 1);
+  // localStorage.setItem("ProductList", JSON.stringify(products));
+  renderProducts(products);
+}
+
 function updateAccounts(pos) {
   accounts.splice(pos, 1);
   localStorage.setItem("AccountList", JSON.stringify(accounts));
   renderAccounts(accounts);
 }
 
+function ManageControlHidden(flag) {
+  if (flag)
+    document.querySelector(".setting-section__manage-wrapper").setAttribute("aria-hidden", "true");
+  else
+  document.querySelector(".setting-section__manage-wrapper").setAttribute("aria-hidden", "false");
+} 
+
 if (page == "user" || page === null) {
+  addProductBtn.style.display = "none";
   type.innerHTML = "Users";
   renderAccounts(accounts);
+}
+else if (page == "product") {
+  addProductBtn.style.display = "block";
+  type.innerHTML = "Products";
+  renderProducts(products);
 }
